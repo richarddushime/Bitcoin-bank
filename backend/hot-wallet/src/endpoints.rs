11b1a6aa6cec4e0fc7f7303658d0f37b4bab372e
itorod <itoroukpong1@gmail.com>
2024-03-21 14:04:03 +0000

    adding api implementations

---
+++ backend/hot-wallet/src/endpoints.rs
@@ -0,0 +1,111 @@
+use actix_web::{get, post, delete, put, HttpResponse, Responder, web};
+use crate::database;
+use crate::schema::{Users,UserAccountDetails,UserSpendHistory};
+use web::{Path, Json};
+
+#[get("bitcoinbank/balance/{user_id}")]
+pub async fn get_user_account_details_by_id(id: Path<i32>) -> impl Responder {
+    let user_id = id.into_inner();
+    
+    match database::get_user_account_details(user_id).unwrap() {
+        Some(user_account_details_from_db) => {
+            HttpResponse::Ok().content_type("application/json").json(user_account_details_from_db)
+        },
+        None => {
+            HttpResponse::NotFound().body(format!("There is no user detail with id {}", user_id))
+        }
+    }
+}
+
+#[get("bitcoinbank/spending/{user_id}")]
+pub async fn get_user_spending_history_by_id(id: Path<i32>) -> impl Responder {
+    let user_id = id.into_inner();
+    
+    match database::get_user_spending_history(user_id.clone()).unwrap() {
+        Some(user_spending_history_from_db) => {
+            HttpResponse::Ok().content_type("application/json").json(user_spending_history_from_db)
+        },
+        None => {
+            HttpResponse::NotFound().body(format!("There is no spending history with id {}", user_id))
+        }
+    }
+}
+
+#[get("bitcoinbank/users")]
+pub async fn get_users() -> impl Responder {
+   
+    match database::get_users().unwrap() {
+        Some(users_list) => {
+            HttpResponse::Ok().content_type("application/json").json(users_list)
+        },
+        None => {
+            HttpResponse::NotFound().body(format!("There is no users"))
+        }
+    }
+}
+
+#[get("bitcoinbank/bankbalance")]
+pub async fn get_bank_balance() -> impl Responder {
+   
+    match database::get_bank_balance().unwrap() {
+        Some(bank_balance_list) => {
+            HttpResponse::Ok().content_type("application/json").json(bank_balance_list)
+        },
+        None => {
+            HttpResponse::NotFound().body(format!("There is no balance available"))
+        }
+    }
+}
+
+#[post("bitcoinbank/signup")]
+pub async fn insert_user(user: Json<Users>) -> impl Responder {
+    match database::insert_user(user.into_inner()) {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
+        Err(_) => {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[post("bitcoinbank/spend")]
+pub async fn insert_user_spend(user_spending: Json<UserSpendHistory>) -> impl Responder {
+    match database::insert_user_spend(user_spending.into_inner()) {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
+        Err(_) => {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[post("bitcoinbank/balance")]
+pub async fn insert_user_account_details(user_account_details: Json<UserAccountDetails>) -> impl Responder {
+    match database::insert_user_account_details(user_account_details.into_inner()) {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
+        Err(_) => {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[put("bitcoinbank/balanceupdate")]
+pub async fn update_user_account_details(user_account_details: Json<UserAccountDetails>) -> impl Responder {
+    let updated_user_account_details = user_account_details.into_inner();
+    match database::update_user_account_details(updated_user_account_details.clone()) {
+        Ok(succeeded) => {
+            if succeeded {
+                HttpResponse::Ok().finish()
+            } else {
+                HttpResponse::NotFound().body(format!("There is no user account detail with id {}", updated_user_account_details.user_id))
+            }
+        }
+        Err(_) => {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
--- backend/hot-wallet/src/main.rs
+++ backend/hot-wallet/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
+mod database;
+mod schema;
+mod endpoints;
+
+use actix_web::{App, HttpServer};
+use env_logger::Env;
+use actix_web::middleware::Logger;
+
+use crate::endpoints::{
+    get_user_account_details_by_id, get_user_spending_history_by_id,
+    insert_user, insert_user_spend,
+    insert_user_account_details,update_user_account_details,get_users,get_bank_balance
+};
+
+
+
+#[actix_web::main]
+async fn  main() -> std::io::Result<()>{
     println!("Hello, world!");
+    //let pool = database::init_pool();
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(move || {
+        App::new()
+            .service(get_user_account_details_by_id)
+            .service(get_user_spending_history_by_id)
+            .service(insert_user)
+            .service(insert_user_spend)
+            .service(insert_user_account_details)
+            .service(update_user_account_details)
+            .service(get_users)
+            .service(get_bank_balance)
+            .wrap(Logger::default())
+    })
+        .bind(("0.0.0.0", 3000))?
+        .workers(2)
+        .run()
+        .await
 }
