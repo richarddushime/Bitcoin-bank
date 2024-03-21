79feef25fcdc29c9cadb3c4cae76e11f5a5ea251
448 OG <superuser@448.africa>
2024-03-21 13:29:46 +0300

    Add ability to send some amount
    Fix panicking when wallet is already loaded

--- backend/common-operations/src/main.rs
+++ backend/common-operations/src/main.rs
@@ -8,13 +8,14 @@ fn main() {
 
     dbg!(&wallet.does_wallet_exist());
     //dbg!(&wallet.load_wallet());
+    /*dbg!(&wallet.get_balance());
+    dbg!(&wallet.generate_blocks(100));
     dbg!(&wallet.get_balance());
-    dbg!(&wallet.generate(100));
-    dbg!(&wallet.get_balance());
-    dbg!(&wallet.generate(10));
+    dbg!(&wallet.generate_blocks(10));*/
     dbg!(&wallet.get_balance());
 
-    dbg!(&wallet.list_unspent());
-
-    dbg!(&wallet.get_p2wsh_address());
+    let address = "bcrt1p5aafnyuujnsq26ljcufdlnrws7ddh22067sjn23mwgmmh5ksmgqqwekq3z";
+    let tx = wallet.send_amount(address, 400_000).unwrap();
+    dbg!(&tx);
+    //dbg!(&wallet.get_p2wsh_address());
 }
--- backend/common-operations/src/wallet.rs
+++ backend/common-operations/src/wallet.rs
@@ -1,10 +1,11 @@
 use bitcoincore_rpc::{
     bitcoin::{
         address::{self, ParseError},
-        Address, Amount, BlockHash, Network,
+        Address, Amount, BlockHash, Network, Txid,
     },
     bitcoincore_rpc_json::{
-        AddressType, GetNetworkInfoResult, ListUnspentResultEntry, LoadWalletResult,
+        AddressType, CreateRawTransactionInput, EstimateMode, GetNetworkInfoResult,
+        ListUnspentResultEntry, LoadWalletResult,
     },
     Auth, Client, Result as BtcResult, RpcApi,
 };
@@ -13,6 +14,7 @@ use std::{
     fs::File,
     io::{self, Read},
     path::Path,
+    str::FromStr,
 };
 
 pub struct Wallet {
@@ -56,31 +58,28 @@ impl Wallet {
         Ok(hash.to_string())
     }
 
-    pub fn does_wallet_exist(&self) -> BtcResult<()> {
-        let wallets = self.client.list_wallets()?;
-        let mut wallet_names = Vec::<String>::new();
+    pub fn does_wallet_exist(&self) {
+        match self.load_wallet() {
+            Ok(_) => println!("WALLET {} was loaded", self.wallet_name),
+            Err(error) => {
+                let error_str = error.to_string();
 
-        wallets.iter().for_each(|wallet| {
-            let path = Path::new(wallet);
-            wallet_names.push(path.file_name().unwrap().to_string_lossy().to_string());
-        });
+                if error_str.contains("is already loaded") {
+                    println!("WALLET ALREADY LOADED");
+                } else {
+                    println!("WALLET NOT FOUND, CREATING NEW ONE");
 
-        if !wallet_names.is_empty() {
-            println!("WALLETS FOUND: {:?}", &wallet_names);
+                    let wallet_outcome = self.create_wallet(self.wallet_name.as_str()).unwrap();
+
+                    println!(
+                        "Wallet {} created with Outcome: {:?}",
+                        self.wallet_name, wallet_outcome
+                    );
+
+                    self.load_wallet().unwrap();
+                }
+            }
         }
-
-        if !wallet_names.contains(&self.wallet_name) {
-            println!("WALLET NOT FOUND, CREATING NEW ONE");
-
-            let wallet_outcome = self.create_wallet(self.wallet_name.as_str()).unwrap();
-
-            println!(
-                "Wallet {} created with Outcome: {:?}",
-                self.wallet_name, wallet_outcome
-            );
-        }
-
-        Ok(())
     }
 
     pub fn create_wallet(&self, name: &str) -> BtcResult<LoadWalletResult> {
@@ -104,8 +103,6 @@ impl Wallet {
     }
 
     pub fn parse_address(&self, address: &str) -> Result<Address, String> {
-        use core::str::FromStr;
-
         match Address::from_str(address) {
             Ok(parsed) => match parsed.require_network(Network::Regtest) {
                 Ok(value) => Ok(value),
@@ -121,12 +118,7 @@ impl Wallet {
             .unwrap()
     }
 
-    pub fn load_wallet(&self) {
-        let load_wallet = self.client.load_wallet(self.wallet_name.as_str()).unwrap();
-        dbg!(load_wallet);
-    }
-
-    pub fn generate(&self, num: u64) {
+    pub fn generate_blocks(&self, num: u64) {
         let rpc_port = format!("-rpcport={}", self.rpc_port);
         let data_dir = format!("-datadir={}", self.data_dir.as_str());
         let outcome = std::process::Command::new("bitcoin-cli")
@@ -140,6 +132,28 @@ impl Wallet {
             .expect("failed to execute process");
 
         println!("GENERATED BLOCKS OUTCOME: \n {:#?}", outcome);
+    }
+
+    pub fn send_amount(&self, address: &str, amount: u64) -> BtcResult<String> {
+        let address = self.parse_address(address).unwrap();
+        let amount = Amount::from_sat(amount);
+
+        let tx = self.client.send_to_address(
+            &address,
+            amount,
+            None,
+            None,
+            None,
+            None,
+            None,
+            Some(EstimateMode::Conservative),
+        )?;
+
+        Ok(tx.to_string())
+    }
+
+    pub fn load_wallet(&self) -> BtcResult<LoadWalletResult> {
+        self.client.load_wallet(&self.wallet_name)
     }
 }
 
