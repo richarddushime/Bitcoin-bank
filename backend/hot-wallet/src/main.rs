ba017d051dde541b2a28b6c69b85b7f45044b132
richarddushime <mudaherarich@gmail.com>
2024-03-27 08:55:58 +0300

    Unused statemts removed

--- backend/hot-wallet/src/main.rs
+++ backend/hot-wallet/src/main.rs
@@ -46,7 +46,6 @@ lazy_static::lazy_static! {
         }
     });
 
-    println!("Hello, world!");
     //let pool = database::init_pool();
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
