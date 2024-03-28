f79ddedc646781fd4fc575049422b8e4d89b3fa3
richarddushime <mudaherarich@gmail.com>
2024-03-28 22:39:27 +0300

    modified font

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -49,7 +49,7 @@ export default {
 
 <style>
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
